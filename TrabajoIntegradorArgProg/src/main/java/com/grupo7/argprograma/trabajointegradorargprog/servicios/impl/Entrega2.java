edcc49266950e2a6ea05b874808bb2c08199fa4c
Xhemanuelv <xhemanuelv@gmail.com>
2023-04-28 21:40:27 -0300

    correccion

--- TrabajoIntegradorArgProg/src/main/java/com/grupo7/argprograma/trabajointegradorargprog/servicios/impl/Entrega2.java
+++ TrabajoIntegradorArgProg/src/main/java/com/grupo7/argprograma/trabajointegradorargprog/servicios/impl/Entrega2.java
@@ -1,9 +1,17 @@
 package com.grupo7.argprograma.trabajointegradorargprog.servicios.impl;
 
+import com.grupo7.argprograma.trabajointegradorargprog.servicios.LeerArchivos;
+
 /**
  *
  * @author Emanuel Villarin
  */
 public class Entrega2 {
+
+    private final LeerArchivos readFile = new LeerArchivosImpl();
+
+    public void mostrarPronosticoDesdeArchivo() {
+        readFile.leerPronostico("pronostico.csv");
+    }
 
 }
